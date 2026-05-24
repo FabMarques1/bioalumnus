fefe6ba611cb4cad0ff0d22de920f893c18f9c85
Fabricio Henrique Marques Olimpio <fabricioolimpio734@gmail.com>
2026-05-24 20:45:33 -0300

    não sei o que colocar

--- profile.php
+++ profile.php
@@ -85,7 +85,7 @@ if(!isset($_SESSION['auth'])){
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-    <link rel="stylesheet" href="frontend/styles/profile.css?v=2">
+    <link rel="stylesheet" href="frontend/styles/profile.css">
     <link rel="icon" href="frontend/assets/icons/icon.png">
 </head>
 <body>
